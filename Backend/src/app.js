ebff56320556f39222cd6c4b4e399f088f91604a
atithiswain09 <atithiswain371@gmail.com>
2026-01-12 21:44:37 +0530

    feat: Update CORS configuration to use environment variable for frontend URL and remove unused .env file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const ai=require("../src/routes/ai.routes");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
